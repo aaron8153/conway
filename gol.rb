d35cfc278f8c859b20b9bc38d7d53e9b435d65e5
Aaron Jacobs <aaron8153@gmail.com>
2013-08-12 00:47:26 -0500

    all four rules implemented...now to set up rendering

--- gol.rb
+++ gol.rb
@@ -12,14 +12,139 @@ class Game
 				end
 	end
 
+	def num_neighbours(cell)
+		neighbours = 0
+		# y, x
+		#-1,-1 NW - not at left or top
+		if cell.x > 0 && cell.y > 0
+			if self.board[cell.x - 1][cell.y - 1].living?
+				neighbours += 1
+			end
+		end
+		#-1,0 N - not at top
+		if cell.x > 0
+			if self.board[cell.x - 1][cell.y].living?
+				neighbours += 1
+			end
+		end
+		#-1,+1 NE - not at top or right
+		if cell.x > 0 && cell.y < ( cols - 1 )
+			if self.board[cell.x - 1][cell.y + 1].living?
+				neighbours += 1
+			end
+		end
+		#0,-1 W - not at left
+		if cell.y > 0
+			if self.board[cell.x][cell.y - 1].living?
+				neighbours += 1
+			end
+		end
+		#0,+1 E - not at right
+		if cell.y < ( cols - 1 )
+			if self.board[cell.x][cell.y + 1].living?
+				neighbours += 1
+			end
+		end
+		#+1,-1 SW - not at left or bottom
+		if cell.x > 0 && cell.x < ( rows - 1 )
+			if self.board[cell.x + 1][cell.y - 1].living?
+				neighbours += 1
+			end
+		end
+		#+1,0 S - not at bottom
+		if cell.x < ( rows - 1 )
+			if self.board[cell.x + 1][cell.y].living?
+				neighbours += 1
+			end
+		end
+		#+1,+1 SE - not at bottom or right
+		if cell.x < ( rows - 1 ) && cell.y < ( cols - 1 )
+			if self.board[cell.x + 1][cell.y + 1].living?
+				neighbours += 1
+			end
+		end
+		neighbours
+	end
+
 end
 
 class Cell
-	attr_accessor :x, :y, :living
+	attr_accessor :x, :y, :living, :will_live
 	
-	def initialize(x, y)	
+	def initialize(x=0, y=0)	
 		@x = x
 		@y = x
 		@living = false
+		@will_live = false
+	end
+
+	def living?
+		living
+	end
+
+	def dead?
+		!living
+	end
+
+	def kill!
+		@living = false
+	end
+
+	def resurect!
+		@living = true
 	end
 end
+
+class Sim
+	attr_accessor :game, :init_cells
+
+	def initialize(game=Game.new, init_cells=[])
+		@game = game
+		@init_cells = init_cells
+
+		init_cells.each_with_index do |r, row|
+			r.each_with_index do |c, col|
+				if init_cells[row][col] == 1 
+					game.board[row][col].living = true
+				end
+			end
+		end
+	end
+
+	def step!
+		game.board.each do |row|
+			row.each do |cell|
+				#Rule 1
+				if cell.living? && game.num_neighbours(cell) < 2
+					cell.will_live = false
+				end
+				#Rule 2
+				if cell.living? && ([2, 3].include? game.num_neighbours(cell))
+					cell.will_live = true
+				end
+				#Rule 3
+				if cell.living? && game.num_neighbours(cell) > 3
+					cell.will_live = false
+				end
+				#Rule 4
+				if cell.dead? && game.num_neighbours(cell) == 3
+					cell.will_live = true
+				end
+			end
+		end
+		game.board.each do |row|
+			row.each do |cell|
+				cell.living = cell.will_live
+			end
+		end
+	end
+end
+
+
+
+
+
+
+
+
+
--- gol_tests.rb
+++ gol_tests.rb
@@ -2,6 +2,10 @@ require 'rspec'
 require_relative 'gol.rb'
 
 describe 'Game of Life' do
+
+	#game obj to be used in tests will init with 10x10 grid
+	let!(:game) { Game.new }
+
 	context 'Game' do
 		subject { Game.new }
 		
@@ -19,6 +23,63 @@ describe 'Game of Life' do
 
 		it 'should be able to access board method' do
 			subject.should respond_to(:board)
+		end
+
+		it 'should be able to access num_neighbours method' do
+			subject.should respond_to(:num_neighbours)
+		end
+
+		it 'should detect a neighbour (-1, -1)' do
+			subject.board[0][0].living = true
+			subject.board[0][0].should be_living
+			subject.num_neighbours(subject.board[1][1]).should == 1
+		end
+		it 'should detect a neighbour (-1, 0)' do
+			subject.board[0][1].living = true
+			subject.board[0][1].should be_living
+			subject.num_neighbours(subject.board[1][1]).should == 1
+		end
+		it 'should detect a neighbour (-1, +1)' do
+			subject.board[0][2].living = true
+			subject.board[0][2].should be_living
+			subject.num_neighbours(subject.board[1][1]).should == 1
+		end
+		it 'should detect a neighbour (0, -1)' do
+			subject.board[1][0].living = true
+			subject.board[1][0].should be_living
+			subject.num_neighbours(subject.board[1][1]).should == 1
+		end
+		it 'should detect a neighbour (0, +1)' do
+			subject.board[1][2].living = true
+			subject.board[1][2].should be_living
+			subject.num_neighbours(subject.board[1][1]).should == 1
+		end
+		it 'should detect a neighbour (+1, -1)' do
+			subject.board[2][0].living = true
+			subject.board[2][0].should be_living
+			subject.num_neighbours(subject.board[1][1]).should == 1
+		end
+		it 'should detect a neighbour (+1, 0)' do
+			subject.board[2][1].living = true
+			subject.board[2][1].should be_living
+			subject.num_neighbours(subject.board[1][1]).should == 1
+		end
+		it 'should detect a neighbour (+1, +1)' do
+			subject.board[2][2].living = true
+			subject.board[2][2].should be_living
+			subject.num_neighbours(subject.board[1][1]).should == 1
+		end
+		it 'should detect all neighbours at once' do
+			subject.board[0][0].living = true
+			subject.board[0][1].living = true
+			subject.board[0][2].living = true
+			subject.board[1][0].living = true
+			subject.board[1][2].living = true
+			subject.board[2][0].living = true
+			subject.board[2][1].living = true
+			subject.board[2][2].living = true
+
+			subject.num_neighbours(subject.board[1][1]).should == 8
 		end
 
 		it 'should make a grid - Array(Array())' do
@@ -51,13 +112,160 @@ describe 'Game of Life' do
 
 		it 'should be able to access living method' do
 			subject.should respond_to(:living)
+		end
+
+		it 'should be able to access will_live method' do
+			subject.should respond_to(:will_live)
+		end
+
+		it 'should initialize will_live to false' do
+			subject.will_live.should be_false
+		end
+
+		it 'should be able to access living? method' do
+			subject.should respond_to(:living?)
 		end	
 
 		it 'should make a dead cell' do
 			subject.living.should be_false
 		end	
 
-		
+		it 'should initialize properly' do
+			subject.x.should == 0
+			subject.y.should == 0
+		end	
+	end
 
+	context 'Sim' do
+		subject { Sim.new }
+		
+		it 'should create a new cell obj' do
+			subject.is_a?(Sim).should be_true
+		end
+
+		it 'should be able to access game method' do
+			subject.should respond_to(:game)
+		end
+
+		it 'should be able to access init_cells method' do
+			subject.should respond_to(:init_cells)
+		end
+
+		it 'should initialize properly' do
+			subject.game.is_a?(Game).should be_true
+			subject.init_cells.is_a?(Array).should be_true
+		end
+
+		it 'should be initialized with cells that are living' do
+			#assuming a 10x10 grid, 0 = dead, 1 = alive
+			cells = [[0,1,0,0,0,0,0,0,0,0],
+					 [0,1,0,0,0,0,0,0,0,0],
+					 [0,1,0,0,0,0,0,0,0,0],
+					 [0,0,0,0,0,0,0,0,0,0],
+					 [0,0,0,0,0,0,0,0,0,0],
+					 [0,0,0,0,0,0,0,0,0,0],
+					 [0,0,0,0,0,0,0,0,0,0],
+					 [0,0,0,0,0,0,0,0,0,0],
+					 [0,0,0,0,0,0,0,0,0,0],
+					 [0,0,0,0,0,0,0,0,0,0]]
+			sim = Sim.new(game, cells)
+			game.board[0][1].should be_living
+			game.board[1][1].should be_living
+			game.board[2][1].should be_living
+		end
+	end
+
+	context 'Rules' do
+		let!(:sim) { Sim.new }
+		
+		context 'Any live cell with fewer than two live neighbours dies, as if caused by under-population.' do
+    		it 'should die with < 1 neighbours' do
+    			cells = [[1,1,0,0,0,0,0,0,0,0],
+						 [0,0,0,0,0,0,0,0,0,0],
+						 [0,0,0,0,0,0,0,0,0,0],
+						 [0,0,0,0,0,0,0,0,0,0],
+						 [0,0,0,0,0,0,0,0,0,0],
+						 [0,0,0,0,0,0,0,0,0,0],
+						 [0,0,0,0,0,0,0,0,0,0],
+						 [0,0,0,0,0,0,0,0,0,0],
+						 [0,0,0,0,0,0,0,0,0,0],
+						 [0,0,0,0,0,0,0,0,0,0]]
+				sim = Sim.new(game, cells)
+				sim.step!
+				game.board[0][0].should be_dead
+				game.board[0][1].should be_dead
+    		end
+    	end
+    	context 'Any live cell with two or three live neighbours lives on to the next generation.' do
+    		it 'should live if it has 2 or 3 neighbours' do
+    			cells = [[1,1,1,0,0,0,0,0,0,0],
+						 [0,1,0,0,0,0,0,0,0,0],
+						 [0,0,0,0,0,0,0,0,0,0],
+						 [0,0,0,0,0,0,0,0,0,0],
+						 [0,0,0,0,0,0,0,0,0,0],
+						 [0,0,0,0,0,0,0,0,0,0],
+						 [0,0,0,0,0,0,0,0,0,0],
+						 [0,0,0,0,0,0,0,0,0,0],
+						 [0,0,0,0,0,0,0,0,0,0],
+						 [0,0,0,0,0,0,0,0,0,0]]
+				sim = Sim.new(game, cells)
+				sim.step!
+				game.board[0][0].should be_living
+				game.board[0][1].should be_living
+				game.board[0][2].should be_living
+				game.board[1][1].should be_living
+    		end
+    	end
+    	context 'Any live cell with more than three live neighbours dies, as if by overcrowding.' do
+    		it 'should die with > 3 neighbours' do
+    			cells = [[1,1,0,0,0,0,0,0,0,0],
+						 [1,1,0,0,0,0,0,0,0,0],
+						 [0,1,0,0,0,0,0,0,0,0],
+						 [0,0,0,0,0,0,0,0,0,0],
+						 [0,0,0,0,0,0,0,0,0,0],
+						 [0,0,0,0,0,0,0,0,0,0],
+						 [0,0,0,0,0,0,0,0,0,0],
+						 [0,0,0,0,0,0,0,0,0,0],
+						 [0,0,0,0,0,0,0,0,0,0],
+						 [0,0,0,0,0,0,0,0,0,0]]
+				sim = Sim.new(game, cells)
+				sim.step!
+				game.board[1][1].should be_dead
+				game.board[1][0].should be_dead
+    		end
+    	end
+    	context 'Any dead cell with exactly three live neighbours becomes a live cell, as if by reproduction.' do
+    		it 'should re-animate with exactly 3 neighbours' do
+    			cells = [[1,1,0,0,0,0,0,0,0,0],
+						 [1,0,0,0,0,0,0,0,0,0],
+						 [0,0,0,0,0,0,0,0,0,0],
+						 [0,0,0,0,0,0,0,0,0,0],
+						 [0,0,0,0,0,0,0,0,0,0],
+						 [0,0,0,0,0,0,0,0,0,0],
+						 [0,0,0,0,0,0,0,0,0,0],
+						 [0,0,0,0,0,0,0,0,0,0],
+						 [0,0,0,0,0,0,0,0,0,0],
+						 [0,0,0,0,0,0,0,0,0,0]]
+				sim = Sim.new(game, cells)
+				sim.step!
+				game.board[1][1].should be_living
+    		end
+    	end
 	end
 end
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
